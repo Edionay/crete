076a6930848bbba21914aeb63eefcab6bba38d2b
Edionay Aguiar <edionay@gmail.com>
2018-08-17 17:58:11 -0300

    Express foi importado para o servidor.

--- server/servidor.js
+++ server/servidor.js
@@ -1,7 +1,17 @@
-var http = require('http');
+var express = require('express');
+var app = express();
 
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-}).listen(1337, '127.0.0.1');
 
-console.log('Servindo em http://127.0.0.1:1337/');
+app.get('/', function (req, res) {
+    res.send("Olá!")
+});
+
+app.listen(3000);
+console.log('Servindo em http://127.0.0.1:3000/');
+
+// var http = require('http');
+//
+// http.createServer(function (req, res) {
+//     res.writeHead(200, {'Content-Type': 'text/plain'});
+// }).listen(1337, '127.0.0.1');
+//
