4339c9f27383f30053b6693e88f5816e6c0ca4cb
Edionay Aguiar <edionay@gmail.com>
2018-08-23 05:22:02 -0300

    Dependência Joi adicionada ao projeto.

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+const Joi = require('joi');
 const express = require('express');
 const app = express();
+
+app.use(express.json());
 
 const encomendas = [
     {
@@ -45,10 +48,41 @@ app.get('/api/encomendas', (req, res) => {
 });
 
 app.get('/api/encomendas/:id', (req, res) => {
+
     const encomenda = encomendas.find(encomenda => encomenda.id === parseInt(req.params.id));
     if (!encomenda) res.status(404).send('Encomenda não encontrada!');
     res.send(encomenda);
 });
+
+
+app.post('/api/encomendas', (req, res) => {
+
+    const modelo = {
+        id: Joi.number().required(),
+        remetente: {
+            nome: Joi.string(),
+            cep: Joi.string()
+        },
+        destinatario: {
+            nome: Joi.string(),
+            cep: Joi.string()
+        }
+    };
+
+    const resultado = Joi.validate(req.body, modelo);
+    console.log(resultado);
+
+    if (resultado.error) {
+        res.status(400).send(resultado.error.details[0].message);
+        return;
+    }
+
+    const encomenda = req.body;
+    encomendas.push(encomenda);
+    console.log(encomendas);
+    res.send(encomenda);
+});
+
 
 const porta = process.env.PORT || 3000;
 app.listen(porta, () => console.log(`Ouvindo na porta ${porta}...`));
