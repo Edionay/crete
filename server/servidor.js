b58a2c619af56ea69550b4bebdc1576c431c848e
Edionay Aguiar <edionay@gmail.com>
2018-08-23 03:20:41 -0300

    Package.json foi criado.

--- server/servidor.js
+++ server/servidor.js
@@ -1,17 +1,7 @@
-var express = require('express');
-var app = express();
+var http = require('http');
 
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+}).listen(1337, '127.0.0.1');
 
-app.get('/', function (req, res) {
-    res.send("Olá!")
-});
-
-app.listen(3000);
-console.log('Servindo em http://127.0.0.1:3000/');
-
-// var http = require('http');
-//
-// http.createServer(function (req, res) {
-//     res.writeHead(200, {'Content-Type': 'text/plain'});
-// }).listen(1337, '127.0.0.1');
-//
+console.log('Servindo em http://127.0.0.1:1337/');
