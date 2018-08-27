ebb10bcc1feefb08f1f0282dbb3c316c77b9f2e7
Edionay Aguiar <edionay@gmail.com>
2018-08-27 00:55:53 -0300

    Previsão de entrega sendo calculada e mostrada.

--- index.js
+++ index.js
@@ -23,9 +23,34 @@ app.get('/encomendas/:remetente/:destinatario', (req, res) => {
         let body = '';
         resp.on('data', data => body += data);
         resp.on('end', () => {
-            let encomenda = JSON.parse(body);
-            console.log(encomenda.destination_addresses);
-            res.send(body)
+            // JSON.parse(body);
+            let resposta = JSON.parse(body);
+            let encomenda = {};
+            let destinatario = {};
+            let remetente = {};
+
+            destinatario.endereco = resposta.destination_addresses[0];
+            remetente.endereco = resposta.origin_addresses[0];
+
+            encomenda.destinatario = destinatario;
+            encomenda.remetente = remetente;
+            encomenda.distancia = resposta.rows[0].elements[0].distance.value;
+            let diasCorridos = resposta.rows[0].elements[0].duration.value;
+            diasCorridos = diasCorridos / 3600;
+            diasCorridos = diasCorridos / 8;
+
+            encomenda.dataDeEntrega = calcularDataDeEntrega(diasCorridos);
+            encomenda.frete = (encomenda.distancia/1000) * 1.2;
+
+            if (req.params.remetente.substring(0, 1) !== req.params.destinatario.substring(0, 1)) {
+                encomenda.frete = encomenda.frete * 1.3;
+            }
+
+            console.log(encomenda);
+            console.log(diasCorridos);
+
+
+            res.send(encomenda)
         });
     });
 });
@@ -42,3 +67,22 @@ app.post('/encomendas', function (req, res) {
 
 const porta = process.env.PORT || 3000;
 app.listen(porta, () => console.log(`Ouvindo na porta ${porta}...`));
+
+function calcularDataDeEntrega(diasCorridos) {
+    const hoje = new Date();
+    let amanha = new Date(hoje.getTime() + 24*60*60*1000);
+    if (amanha.getDay() === 0) {
+        amanha =  new Date(amanha.getTime() + 24*60*60*1000);
+    }
+
+    let dataDeEntrega = amanha;
+
+    for(var days = 1; days <= diasCorridos; days++) {
+        dataDeEntrega = new Date(amanha.getTime() + (days *24*60*60*1000));
+        if(dataDeEntrega.getDay() === 0) {
+            diasCorridos++;
+        }
+    }
+    return dataDeEntrega;
+}
+
--- public/controllers/encomendasController.js
+++ public/controllers/encomendasController.js
@@ -4,16 +4,21 @@ creteApp.controller('encomendasController', ['$scope', '$http', function($scope,
 
     $http.get('./encomendas').then(function (response) {
         $scope.encomendas = response.data;
-        console.log('edionay')
     }, function (error) {
 
     });
     
     $scope.consultaFrete = function () {
-        console.log($scope.remetente);
+        // console.log($scope.remetente);
 
         $http.get(`/encomendas/${$scope.remetente.cep}/${$scope.destinatario.cep}`).then(function (response) {
             console.log(response.data);
+            $scope.encomenda = response.data;
+            console.log($scope.encomenda.dataDeEntrega);
+            const dataDeEntrega = new Date($scope.encomenda.dataDeEntrega);
+            $scope.diaDeEntrega =  dataDeEntrega.getDate();
+            $scope.mesDeEntrega =  dataDeEntrega.getMonth();
+            $scope.anoDeEntrega =  dataDeEntrega.getFullYear();
         }, function (error) {
 
         });
