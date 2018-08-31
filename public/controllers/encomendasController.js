c1851499d1f26747728b13cccb587f49316d04ee
Edionay Aguiar <edionay@gmail.com>
2018-08-31 17:13:40 -0300

    O resultado da consulta está sendo exibido dinamicamente.

--- public/controllers/encomendasController.js
+++ public/controllers/encomendasController.js
@@ -3,6 +3,7 @@ creteApp.controller('encomendasController', ['$scope', '$http', function($scope,
 
     $scope.remetente = "";
 
+
     $scope.consultaFrete = function () {
 
         $http.get(`/encomendas/${$scope.remetente}/${$scope.destinatario}`).then(function (response) {
@@ -13,9 +14,32 @@ creteApp.controller('encomendasController', ['$scope', '$http', function($scope,
             $scope.diaDeEntrega =  dataDeEntrega.getDate();
             $scope.mesDeEntrega =  dataDeEntrega.getMonth();
             $scope.anoDeEntrega =  dataDeEntrega.getFullYear();
+            exibirResultado();
         }, function (error) {
 
         });
+    };
+
+    function exibirResultado() {
+        let survivorInfo = document.getElementById('resultado');
+        survivorInfo.style.display = 'block';
     }
+    function ocultarResultado() {
+        const survivorInfo = document.getElementById('resultado');
+        survivorInfo.style.display = 'none';
+    }
+
+    function exibirLoader() {
+        const loader = document.getElementById('loader');
+        survivorInfo.style.display = 'none';
+    }
+
+    function ocultarLoader() {
+        const loader = document.getElementById('loader');
+        survivorInfo.style.display = 'none';
+    }
+
+    ocultarResultado();
+
 
 }]);
